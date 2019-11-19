d580ccd157c00325469b1953bcfb3516f3efdf66
95CivicSi <aaron_salyer@yahoo.com>
2019-11-18 20:08:55 -0500

    Server: Add missing $mainInterface variable.

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -82,6 +82,7 @@ use pocketmine\nbt\tag\IntTag;
 use pocketmine\nbt\tag\LongTag;
 use pocketmine\nbt\tag\ShortTag;
 use pocketmine\nbt\tag\StringTag;
+use pocketmine\network\AdvancedSourceInterface;
 use pocketmine\network\Network;
 use pocketmine\network\protocol\BatchPacket;
 use pocketmine\network\protocol\CraftingDataPacket;
@@ -240,6 +241,9 @@ class Server{
 
 	/** @var LevelMetadataStore */
 	private $levelMetadata;
+
+	/** @var AdvancedSourceInterface */
+	private $mainInterface;
 
 	/** @var Network */
 	private $network;
